f047675348a35b5d2e211fa1d574211f4c7f6ab1
Pablo George <pablogeokar@hotmail.com>
2021-06-11 15:22:13 -0300

    Adicionando responsividade no componente

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,14 +1,31 @@
 import styled from 'styled-components'
 
+// Configurações iniciais
+const width = '300px'
+
 const Container = styled.div`
-  position: absolute;
-  height: 100vh;
-  width: 300px;
+  position: fixed;
   left: 0;
   top: 0;
+  height: 100vh;
+  width: ${width};  
   background-color: ${({ theme }) => theme.colors.primary};
   color: #fff;
   overflow: hidden ;
+  transition: width 300ms;
+
+  @media only screen and (max-width: 1200px) {
+    width: 70px;
+
+    &:hover {
+      width: ${width};
+      z-index: 200;
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    left: -100% !important;
+  }
 `
 
 export default function Sidebar() {
