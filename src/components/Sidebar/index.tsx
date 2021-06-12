30be82a936723f6a02ea477389b7bdf65e8c994b
Pablo George <pablogeokar@hotmail.com>
2021-06-11 21:37:39 -0300

    Responsividade na sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,7 +1,59 @@
 import styled from 'styled-components'
 
 // Configurações iniciais
-const width = '300px'
+const width = '280px'
+
+const Brand = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;  
+  height: 90px;
+  padding: 1rem;
+
+  @media only screen and (max-width: 1200px) {
+    padding-left: 1rem;
+    text-align: center;
+
+    img:last-child{
+      display: none;
+    }
+  }
+`
+
+const Menu = styled.ul`
+  margin-top: 1rem;
+`
+
+const Item = styled.li`    
+  width: 100%;
+  margin-bottom: 1.7rem;
+  padding-left: 2rem;
+
+  a{
+    display: flex;
+    align-items: center;
+    padding-left: 1rem;
+    display: block;    
+    font-size: 1.1rem;
+  }
+
+  a.active {
+  background: #fff;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  color: ${({ theme }) => theme.colors.primary};
+  border-radius: 10px 0px 0px 10px;
+}
+
+  a > i{
+    font-size: 1.5rem;
+    padding-right: 1rem;    
+  }
+  a:hover{
+    border-right: 6px solid #0ed1f3;    
+    font-weight:bold;
+  }
+`
 
 const Container = styled.div`
   position: fixed;
@@ -20,6 +72,38 @@ const Container = styled.div`
     &:hover {
       width: ${width};
       z-index: 200;
+
+      ${Brand} {        
+        padding-left: 2rem;
+        text-align: left;           
+
+        img:last-child{
+        display: inline;
+        } 
+      }
+
+      ${Item} {        
+        padding-left: 2rem;
+        text-align: left; 
+
+        a span:last-child{
+          display: inline;
+        }  
+      }        
+    }
+
+    ${Item}{
+      a{
+        padding-left: 0;
+
+        span:last-child{
+          display: none;
+        }
+      }
+
+      i{
+        padding-left: .2rem;
+      }
     }
   }
 
@@ -31,7 +115,60 @@ const Container = styled.div`
 export default function Sidebar() {
   return (
     <Container>
-      Menu
+      <Brand>
+        <img src="/imgs/brand.svg" alt="" />
+        <img src="/imgs/brand-text.svg" alt="" width="150" />
+      </Brand>
+      <Menu>
+        <Item>
+          <a href="#" className="active">
+            <i className="las la-igloo"></i>
+            <span>Início</span>
+          </a>
+        </Item>
+        <Item>
+          <a href="#" >
+            <i className="las la-users"></i>
+            <span>Cadastros</span>
+          </a>
+        </Item>
+        <Item>
+          <a href="#" >
+            <i className="las la-receipt"></i>
+            <span>Depto. Contábil</span>
+          </a>
+        </Item>
+        <Item>
+          <a href="#" >
+            <i className="las la-clipboard"></i>
+            <span>Depto. Fiscal</span>
+          </a>
+        </Item>
+        <Item>
+          <a href="#" >
+            <i className="las la-clipboard"></i>
+            <span>Depto. Financeiro</span>
+          </a>
+        </Item>
+        <Item>
+          <a href="#" >
+            <i className="las la-shopping-bag"></i>
+            <span>Depto. Pessoal</span>
+          </a>
+        </Item>
+        <Item>
+          <a href="#" >
+            <i className="las la-clipboard-list"></i>
+            <span>Tarefas</span>
+          </a>
+        </Item>
+        <Item>
+          <a href="#" >
+            <i className="las la-user-circle"></i>
+            <span>Usuários</span>
+          </a>
+        </Item>
+      </Menu>
     </Container>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,10 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" href="/favicon.png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+          <link
+            rel="stylesheet"
+            href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
+          />
         </Head>
         <body>
           <Main />
